3050486e667d9efcfce5274b0f47b7714b217663
Victor Hawley Jr <victor@victorhawley.com>
2016-05-01 16:21:55 -0400

    First commit

---
+++ SSLyrics/SSLyrics/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SSLyrics
+//
+//  Created by Victor Hawley on 5/1/16.
+//  Copyright © 2016 Victor Hawley Jr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SSLyrics/SSLyrics/SSLyrics-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SSLyrics-Bridging-Header.h
+//  SSLyrics
+//
+//  Created by Victor Hawley on 5/1/16.
+//  Copyright © 2016 Victor Hawley Jr. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import "iTunes.h"
+
+#endif /* Header_h */
